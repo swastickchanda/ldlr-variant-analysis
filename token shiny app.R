f5e0e5fc73af89dc8196aca5453c327a1a97bce2
swastickchanda <swastickchanda11@gmail.com>
2026-09-14 23:10:23 +0530

    Delete token shiny app.R

--- token shiny app.R
+++
@@ -1,6 +0,0 @@
-install.packages("rsconnect")
-library(rsconnect)
-rsconnect::setAccountInfo(name='swastick',
-                          token='4F102E7CB4AF66AB2236DE6C017653F7',
-                          secret='0HIiTNXrlfF/+tHLs2l8vZewzwQ51IO1JnI9Is3x')
-rsconnect::deployApp()
